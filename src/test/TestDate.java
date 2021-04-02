acd190da9fbfe677d3e157f98acca51ae2461d02
YinZiXian <15525112406@163.com>
2021-04-02 13:58:51 +0800

    20210402

---
+++ src/test/TestDate.java
@@ -0,0 +1,10 @@
+package test;
+
+import java.util.Date;
+
+public class TestDate {
+	public static void main(String[] args) {
+		Date date = new Date();
+		System.out.println(date.toString());
+	}
+}
---
+++ src/test/TestException.java
@@ -0,0 +1,7 @@
+package test;
+
+public class TestException {
+	public static void main(String[] args) {
+
+	}
+}
