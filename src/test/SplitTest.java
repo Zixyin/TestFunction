24b71d833d22660cc868185a29af3c88024d0324
YinZiXian <15525112406@163.com>
2021-06-18 10:03:04 +0800

    20210618

--- src/test/SplitTest.java
+++ src/test/SplitTest.java
@@ -4,5 +4,7 @@ public class SplitTest {
 	public static void main(String[] args) {
 		for(String s : "asd".split(","))
 			System.out.println(s);
+		System.out.println("".split("-")[0]);
+		System.out.println("".split("-")[1]);
 	}
 }
